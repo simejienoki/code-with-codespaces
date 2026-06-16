2fb65883b7754b9edba4cabe08512f4634468fd9
simejienoki <itsukinoko124@fuji.waseda.jp>
2026-06-16 12:11:01 +0000

    fix: correct dev container configuration

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e
 
 sudo apt-get update
-sudo apt-get install -y sl
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y sl
 echo 'export PATH=$PATH:/usr/games' >> ~/.bashrc
 echo 'export PATH=$PATH:/usr/games' >> ~/.zshrc
